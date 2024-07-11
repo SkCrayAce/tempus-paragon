84022d95451f619f243943c29b790510f63ad076
SkCray Ace <skcrayace2525@gmail.com>
2024-07-11 09:38:20 +0800

    Item database + added sprites

---
+++ elements/items/item_database.gd
@@ -0,0 +1,42 @@
+extends Node
+
+#can add property to make a unique effect i think
+#maybe separate rarity of items to different arrays to deal with probability
+
+var items = [
+	#common non-consumables
+	{"type": "Non-consumable", "name": "Flintlock Pistol", "effect": "Additional Damage", "rarity": "Common",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet6.png")},
+	{"type": "Non-consumable", "name": "Steam Helmet", "effect": "Reduce Damage Recieved", "rarity": "Common",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet15.png")},
+	{"type": "Non-consumable", "name": "Mini Steam Engine", "effect": "Extra Max Health", "rarity": "Common",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet12.png")},
+	{"type": "Non-consumable", "name": "Plated Gloves", "effect": "Small Chance to Block", "rarity": "Common",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet7.png")},
+	{"type": "Non-consumable", "name": "Brass Cookie", "effect": "Small Chance to Critical Damage", "rarity": "Common",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet16.png")},
+	
+	#uncommon non-consumables
+	{"type": "Non-consumable", "name": "Cog Ring", "effect": "Become invincible for a short duration after getting hit", "rarity": "Uncommon",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet8.png")},
+	{"type": "Non-consumable", "name": "Anima Ring", "effect": "Grants health regeneration but increases Attack Cooldown", "rarity": "Uncommon",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet8.png")},
+	
+	#rare non-consumables
+	{"type": "Non-consumable", "name": "Shard of Time", "effect": "Reduce attack cooldown greatly but removes critical chance", "rarity": "Rare",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet1.png")},
+	{"type": "Non-consumable", "name": "Shard of Space", "effect": "Doubles AoE attack of character but reduced damage", "rarity": "Rare",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet2.png")},
+	{"type": "Non-consumable", "name": "Shard of Construct", "effect": "Greatly increase damage but reduces AoE", "rarity": "Rare",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet3.png")},
+	{"type": "Non-consumable", "name": "Shard of Life", "effect": "Grants lifesteal equal to damage done, but reduces character health by 40%", "rarity": "Rare",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet10.png")},
+	{"type": "Non-consumable", "name": "Shard of Chaos", "effect": "Greatly increase critical chance but randomizes AoE", "rarity": "Rare",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet11.png")},
+	
+	#epic non-consumables
+	{"type": "Non-consumable", "name": "Clockwork Edge", "effect": "Makes user deal double damage but also take double damage.", "rarity": "Epic",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet18.png")},
+	{"type": "Non-consumable", "name": "Ultima Emblem", "effect": "Grants special ability to damage all enemies in the area but damages 10% of all characters' health.", "rarity": "Epic",  
+	"texture": preload("res://tempus_assets/item sprites/item-spritesheet18.png")}
+]
--- scripts/global.gd
+++ scripts/global.gd
@@ -10,14 +10,84 @@ var dragged_char_name : String
 var enemy_position : Vector2
 var enemy_dict : Dictionary 
 
-# overworld scene vars
-var enemy_dict : Dictionary 
-
 #Overworld Data
 var levels_cleared = 0
 var curr_area = "slums"
-var current_scene : PackedScene
-var player_position : Vector2
+
+#Scene and node references
+var player_node: Node = null
+@onready var inventory_slot_scene = preload("res://scenes/inventory_slot.tscn")
+
+#Inventory Data and Stuff
+var inventory = []
+signal inventory_updated
+
+
+#Inventory Functions------------
+
+func _ready():
+	inventory.resize(12)
+
+func add_item(item):
+	for i in range(inventory.size()):
+		if inventory[i] != null and inventory[i]["type"] == inventory[i]["type"] and inventory[i]["effect"] == inventory[i]["effect"]:
+			#inventory[i]["0quantity"] += item["quantity"]
+			#inventory_updated.emit()
+			#retrun true
+			print("item added", inventory)
+			pass
+		elif inventory[i] == null:
+			inventory[i] = item
+			inventory_updated.emit()
+			print("item added", inventory)
+			return true
+
+
+func remove_item(item_type, item_effect):
+	for i in range(inventory.size()):
+		if inventory[i] != null and inventory[i]["type"] == item_type and inventory[i]["effect"] == item_effect:
+			inventory[i]["quantity"] -= 1
+			if inventory[i]["quantity"] <= 0:
+				inventory[i] = null
+			inventory_updated.emit()
+			return true
+	return false
+
+func increase_inventory_size():
+	inventory_updated.emit()
+
+func set_player_reference(player):
+	player_node = player
+	
+func adjust_drop_position(position):
+	var radius = 30
+	var nearby_items = get_tree().get_nodes_in_group("Items")
+	for item in nearby_items:
+		if item.global_position.distance_to(position) < radius:
+			var random_offset = Vector2(randf_range(-radius, radius), randf_range(-radius, radius))
+			position += random_offset
+			break
+	return position
+
+func drop_item(item_data, drop_position):
+	var item_scene = load(item_data["scene_path"])
+	var item_instance = item_scene.instantiate()
+	item_instance.set_item_data(item_data)
+	drop_position = adjust_drop_position(drop_position)
+	item_instance.global_position = drop_position
+	get_tree().current_scene.add_child(item_instance)
+
+func swap_inventory_items(index1, index2):
+	if index1 < 0 or index1 > inventory.size() or index2 < 0 or index2 > inventory.size():
+		return false
+	
+	var temp = inventory[index1]
+	inventory[index1] = inventory[index2]
+	inventory[index2] = temp
+	print("swap happened")
+	inventory_updated.emit()
+	return true
+#---------------------------------
 
 
 
