b20423e403359a3c8e5ddd546b299d23ad8dc03c
SkCray Ace <skcrayace2525@gmail.com>
2024-07-24 13:02:41 +0800

    starting area spawns random item

--- scripts/slums0.gd
+++ scripts/slums0.gd
@@ -1,7 +1,7 @@
 extends "res://scripts/slums_control.gd"
 
 @onready var player_spawn_point = $PlayerSpawnPoint
-
+@onready var item_spawn_point = $ItemSpawnPoint
 
 
 func _ready():
@@ -10,6 +10,7 @@ func _ready():
 	prints(global.levels_cleared)
 	player_instance.position = player_spawn_point.position
 	add_child(player_instance)
+	global.drop_random_item(item_spawn_point.position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
